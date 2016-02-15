77e8f6c6b33226b1868453aebf388d710e2a2eb0
guilherme_welter <guilherme.a.welter@gmail.com>
2016-02-15 18:53:24 -0200

    Adding missing file

---
+++ BackupZipTool/ViewModel/Services/BackupScheaduler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BackupZipTool.ViewModel.Services
+{
+    public sealed class BackupScheaduler
+    {
+        private static BackupScheaduler instance = null;
+        private static readonly object padlock = new object();
+
+        private static Action externalAction;
+        private Timer timer;
+        private double dailyTimer = 1000 * 60 * 60 * 24; //set interval of one day
+
+        BackupScheaduler() {
+            timer = new Timer();
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+
+            syncHour();
+
+            timer.Start();
+        }
+
+        public static BackupScheaduler Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new BackupScheaduler();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void addAction(Action backupZip)
+        {
+            externalAction = backupZip;
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+            externalAction();
+            timer.Interval = dailyTimer;
+            timer.Start();
+        }
+
+        private void syncHour()
+        {
+            TimeSpan nextWork = DateTime.Today.AddDays(1) - DateTime.Now;
+            timer.Interval = nextWork.TotalMilliseconds;
+        }
+    }
+}
